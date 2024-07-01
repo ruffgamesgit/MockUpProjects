f7031dfa014aa17eda2e4b018b7f6b0fa80dddc1
Ataberk Atik <onur@roof.games>
2024-07-01 15:09:57 +0300

    art update

--- Assets/Game/Shaders/CustomFuction/Lighting.hlsl
+++ Assets/Game/Shaders/CustomFuction/Lighting.hlsl
@@ -1,18 +1,19 @@
 void MainLight_half(half3 WorldPos, out half3 Direction, out half3 Color,
-	out half DistanceAtten, out half ShadowAtten)
+    out half DistanceAtten, out half ShadowAtten)
 {
 #ifdef SHADERGRAPH_PREVIEW
     Direction = half3(0.5, 0.5, 0);
-    Color = 1;
-    DistanceAtten = 1;
-    ShadowAtten = 1;
+    Color = half3(1.0, 1.0, 1.0);
+    DistanceAtten = 1.0;
+    ShadowAtten = 1.0;
 #else
+    half4 shadowCoord;
     
 #if SHADOWS_SCREEN
-	half4 clipPos = TransformWorldToHClip(WorldPos);
-	half4 shadowCoord = ComputeScreenPos(clipPos);
+    half4 clipPos = TransformWorldToHClip(WorldPos);
+    shadowCoord = ComputeScreenPos(clipPos);
 #else
-	half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    shadowCoord = TransformWorldToShadowCoord(WorldPos);
 #endif
 
     Light mainLight = GetMainLight(shadowCoord);
